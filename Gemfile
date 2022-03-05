2aa94a8c7681427174259a08411cf435178a5529
Marcus Parker <parkemg@gmail.com>
2022-03-04 22:28:38 -0600

    added users and posts connection, troubleshooting

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'byebug'
   gem 'rspec-rails', '~>3.0'
   gem 'database_cleaner'
+  gem 'faker'
 end
 
 group :development do
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,35 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show]
-    
-    def index
-    
-    end
+  before_action :set_post, only: [:show]
 
-    def new
+  def index
+  end
+
+  def new
     @post = Post.new
+  end
 
-    end
-
-    def create
+  def create
+   
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
     if @post.save
-       redirect_to @post, notice: 'Your post was created successfully'
-      else
-        render :new
-   end
+      redirect_to @post, notice: "Your post was created successfully"
+    else
+      render :new
     end
+  end
 
-    def show
-        
-    end
+  def show
+  end
 
-    private
-     def post_params
-        params.require(:post).permit(:date, :rationale)
-     end
+  private
 
-     def set_post
-        @post = Post.find(params[:id]) 
-    end
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
